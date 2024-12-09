98d8fee4f99c7d8d3759d24b92f3d887ee855680
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-12-09 19:04:14 +0100

    add ui.c/h and example.c/h

---
+++ examples/example.h
@@ -0,0 +1,50 @@
+/**
+ * @file example.h
+ *
+ */
+
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*Include all the widget and components of this library*/
+#include "dark_slider/dark_slider.h"
+#include "slider_box/slider_box.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL VARIABLES
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * Initialize the component library
+ */
+void example_init(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*EXAMPLE_H*/
