060aadf58b0cdefc2355e8ca9c4e4bcd3f8be3db
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-12-11 14:54:24 +0100

    minor fixes

--- examples/widgets/dark_slider/dark_slider_gen.c
+++ examples/widgets/dark_slider/dark_slider_gen.c
@@ -73,60 +73,40 @@ static void dark_slider_constructor(const lv_obj_class_t * class_p, lv_obj_t * o
     static lv_style_t style_main;
     
     static lv_style_t style_light_knob;
-static lv_style_t style_blue_indicator;
+    static lv_style_t style_blue_indicator;
 
     static bool style_main_inited = false;
 
     if(!style_main_inited) {
-
-    
-
         lv_style_init(&style_main);
-            lv_style_set_bg_color(&style_main, lv_color_hex(0x333333));
-
-    lv_style_set_bg_opa(&style_main, 255);
-
-    lv_style_set_border_width(&style_main, 2);
-
-    lv_style_set_border_color(&style_main, lv_color_hex(0xaaaaaa));
-
-    lv_style_set_radius(&style_main, 100);
-
-    lv_style_set_width(&style_main, 120);
-
-    lv_style_set_height(&style_main, 16);
-
-
+        lv_style_set_bg_color(&style_main, lv_color_hex(0x333333));
+        lv_style_set_bg_opa(&style_main, 255);
+        lv_style_set_border_width(&style_main, 2);
+        lv_style_set_border_color(&style_main, lv_color_hex(0xaaaaaa));
+        lv_style_set_radius(&style_main, 100);
+        lv_style_set_width(&style_main, 120);
+        lv_style_set_height(&style_main, 16);
 
         lv_style_init(&style_light_knob);
-            lv_style_set_bg_color(&style_light_knob, lv_color_hex(0xeeeeee));
-
-    lv_style_set_bg_opa(&style_light_knob, 255);
-
-    lv_style_set_border_width(&style_light_knob, 2);
-
-    lv_style_set_radius(&style_light_knob, 100);
-
-    lv_style_set_border_color(&style_light_knob, lv_color_hex(0xaaaaaa));
-
-
+        lv_style_set_bg_color(&style_light_knob, lv_color_hex(0xeeeeee));
+        lv_style_set_bg_opa(&style_light_knob, 255);
+        lv_style_set_border_width(&style_light_knob, 2);
+        lv_style_set_radius(&style_light_knob, 100);
+        lv_style_set_border_color(&style_light_knob, lv_color_hex(0xaaaaaa));
 
         lv_style_init(&style_blue_indicator);
-            lv_style_set_bg_color(&style_blue_indicator, lv_color_hex(0x0000ff));
-
-    lv_style_set_bg_opa(&style_blue_indicator, 255);
-
-    lv_style_set_radius(&style_blue_indicator, 100);
-
+        lv_style_set_bg_color(&style_blue_indicator, lv_color_hex(0x0000ff));
+        lv_style_set_bg_opa(&style_blue_indicator, 255);
+        lv_style_set_radius(&style_blue_indicator, 100);
 
         style_main_inited = true;
     }
 
     lv_obj_add_style(obj, &style_main, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_add_style(obj, &style_light_knob, LV_PART_KNOB | LV_STATE_DEFAULT);
+    lv_obj_add_style(obj, &style_blue_indicator, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+    lv_obj_add_flag(obj, LV_OBJ_FLAG_SEND_DRAW_TASK_EVENTS);
     
-    
-    
-
     dark_slider_t * dark_slider = (dark_slider_t *)obj;
     
 
